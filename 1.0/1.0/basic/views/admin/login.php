9dfab431abb3cabb144ab521a8182504c17e0f0f
Your Name <you@example.com>
2018-03-29 18:04:08 +0800

    1.0

--- 1.0/1.0/basic/views/admin/login.php
+++
@@ -1,6 +0,0 @@
-<form action="?r=admin/login" method="post">
-	userName:<input type="text" name="username">
-	userName:<input type="password" name="password">
-	
-	<input type="submit" value="LOGIN">
-</form>
